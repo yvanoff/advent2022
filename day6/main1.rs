6b0b8e445728c1500a2d316ae01c48374df13027
Alexandre Chesneau <alexandre.chesneau@yahoo.fr>
2022-12-06 13:52:24 +0100

    Day 6. Note that the part 1 works by blind luck, it shouldn't work

---
+++ day6/main1.rs
@@ -0,0 +1,36 @@
+use std::fs::File;
+use std::io::{self,BufRead};
+use std::path::Path;
+
+fn main() {
+    let mut char_pos = 0;
+    let mut validated = String::new();
+    if let Ok(lines) = read_lines("input") {
+        for line in lines {
+            if let Ok(ip) = line {
+                for c in ip.chars() {
+                    if (!validated.contains(c)) && (validated.len() == 3) {
+                        char_pos += 1;
+                        break;
+                    } else {
+                        char_pos += 1;
+                        if validated.contains(c) {
+                            validated = String::new();
+                        }
+                        validated.push(c);
+                        if validated.len() > 3 {
+                            validated.remove(0);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    println!("We are looking for char: {}", char_pos);
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
